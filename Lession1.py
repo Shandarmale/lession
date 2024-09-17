e6c95104a66dcfe66e921997ac3ed610ec3b2a2e
Shandarmale <team08@mail.ru>
2024-09-17 13:34:34 +0300

    Add files via upload

---
+++ Lession1.py
@@ -0,0 +1,5 @@
+print(12+89-1)
+print(9**0.5 * 5) #1st program
+print(9.99 > 9.98 and 1000 != 1000.1) #2st program
+print(2*2+2 == 2*(2+2))  #3rd program
+print(123.456 * 10,int(1234.56)) #4th program
