2c42cadbb7e9f237b4d786ead15dc446811d0fd5
Shandarmale <team08@mail.ru>
2024-09-17 14:41:59 +0300

    Переменные

---
+++ variables.py
@@ -0,0 +1,5 @@
+homework_complete = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+Time_for_one_task = (1.5 / 12)
+print(course_name,homework_complete,number_of_hours_spent,Time_for_one_task)
