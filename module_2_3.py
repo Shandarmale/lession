3fa870d8efc7ed2153e955e89360c4ee68c1677b
Shandarmale <team08@mail.ru>
2024-09-30 00:00:28 +0300

    Update module_2_3.py
    исправлено

--- module_2_3.py
+++ module_2_3.py
@@ -1,10 +1,8 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 null_is_nothing = 0
 while null_is_nothing < len(my_list):
-    if my_list[null_is_nothing] >= 0:
-        if my_list[null_is_nothing] != 0:
-            print(my_list[null_is_nothing])
-            null_is_nothing = null_is_nothing + 1
-            continue
-        else:
-            break
+    if my_list[null_is_nothing] < 0:
+        break
+    if my_list[null_is_nothing] > 0:
+        print(my_list[null_is_nothing])
+    null_is_nothing = null_is_nothing + 1
