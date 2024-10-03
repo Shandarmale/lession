763e17ca291a9a4afd2005f299dcfc8f571404c3
Shandarmale <team08@mail.ru>
2024-10-03 16:56:55 +0300

    Update function.py

--- function.py
+++ function.py
@@ -1,21 +1,17 @@
-
-def get_matrix(n,m,value):
+def get_matrix(n, m, value):
     matrix = []
     for i in range(n):
-        rep = [value]*n
+        rep = []
         matrix.append(rep)
-
-        return matrix
-
-
-
-
+        for k in range(m):
+            rep.append(value)
+    return matrix
 
 
 
 result1 = get_matrix(2, 2, 10)
-result2 = get_matrix(5, 3, 42)
-result3 = get_matrix(2, 4, 13)
-print(result1*2)
-print(result2*3)
-print(result3*4)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
