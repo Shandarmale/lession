aa6aeb5b85a67d4edb9f906cd347fb3169a8bdb1
Shandarmale <team08@mail.ru>
2024-10-06 11:50:13 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,26 @@
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string):
+     n = len(string), string.upper(),string.lower()
+     count_calls()
+     return n
+
+
+def is_contains(string,list_to_search):
+    count_calls()
+    return string.upper() in [s.upper() for s in list_to_search]
+
+
+
+
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
