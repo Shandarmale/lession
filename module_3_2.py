1a23e113fcabdaeba92a2b1b5458d2f70c720a0a
Shandarmale <team08@mail.ru>
2024-10-06 21:37:03 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,5 +1,6 @@
 def send_email(message, recipient, *, sender="university.help@gmail.com"):
-    if ("@" not in sender or "@" not in recipient) or (recipient.endswith(".com"or".ru"or".net") and (sender.endswith(".com"or".ru"or".net"))):
+    #if ("@" not in sender and "@" not in recipient) and (recipient.endswith(".com"or".ru"or".net") and (sender.endswith(".com"or".ru"or".net"))):
+    if ("@" not in (sender or recipient) or (".com" or ".ru" or ".net") not in (recipient and sender)):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
     elif recipient == sender:
         print("Нельзя отправить письмо самому себе!")
@@ -7,7 +8,6 @@ def send_email(message, recipient, *, sender="university.help@gmail.com"):
         print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
     else:
         print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
-
 
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
