3be35bf675c6a0643e86721b5f730df6f90313a4
Shandarmale <team08@mail.ru>
2024-10-30 13:18:44 +0300

    Add files via upload

--- module_5_3.py
+++ module_5_3.py
@@ -14,33 +14,30 @@ class House:
     def __str__(self):
         return f"Название: {self.name}, кол-во этажей:{self.number_of_floor}"
     def __eq__(self, other):
-        return isinstance(other, int) and self.number_of_floor == other.number_of_floor
+        if isinstance(other, House):
+            return self.number_of_floor == other.number_of_floor
     def __lt__(self, other):
-        return self.number_of_floor < other.number_of_floor
+        if isinstance(other, House):
+            return self.number_of_floor < other.number_of_floor
     def __le__(self, other):
-        return self.number_of_floor <= other.number_of_floor
+        if isinstance(other, House):
+            return self.number_of_floor <= other.number_of_floor
     def __gt__(self, other):
-        return self.number_of_floor > other.number_of_floor
+        if isinstance(other, House):
+            return self.number_of_floor > other.number_of_floor
     def __ge__(self, other):
-        return self.number_of_floor >= other.number_of_floor
+        if isinstance(other, House):
+            return self.number_of_floor >= other.number_of_floor
     def __ne__(self, other):
         return self.number_of_floor != other.number_of_floor
-    def __add__(self, other):
-        if not isinstance(other, int):
-            return  House(self.number_of_floor + other)
-    def __iadd__(self, other):
-        if isinstance(other, int):
-            return House(self.number_of_floor + other)
-    def __radd__(self, other):
-        if not isinstance(other, (int, House)):
-            nof = other
-        if isinstance(other, House):
-            nof = other.number_of_floor
-            return  House(self.number_of_floor + nof)
-
-
-
-
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floor += value
+            return self
+    def __radd__(self, value):
+        return self + value
+    def __iadd__(self, value):
+        return self + value
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
